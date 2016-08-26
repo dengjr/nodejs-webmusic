d2f48da13c51255d7b25e69b2371722c3360149b
djr <413644942@qq.com>
2016-08-26 09:02:58 +0800

    gf

--- models/music.js
+++
@@ -1,5 +0,0 @@
-var mongoose = require('mongoose');
-var musicSchema = require('../schemas/music');
-var music = mongoose.model('musics',musicSchema);
-
-module.exports = music;
--- models/user.js
+++
@@ -1,5 +0,0 @@
-var mongoose = require('mongoose');
-var userSchema = require('../schemas/user');
-var user = mongoose.model('users',userSchema);
-
-module.exports = user;
